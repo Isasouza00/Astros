6549f0af8301256a2682b37cf21849a49f602efa
Isasouza00 <isasouza20190701@gmail.com>
2024-03-21 09:42:16 -0300

    botão_senha

---
+++ src/pages/cadastro_por_sol/pass.js
@@ -0,0 +1,12 @@
+function viewPass(){
+    var inputPass = document.getElementById('password')
+    var btnShowPass = document.getElementById('btn_pass')
+
+    if(inputPass.type === 'password'){
+        inputPass.setAttribute('type','text')
+        btnShowPass.classList.replace('bi-eye','bi-eye-slash')
+    }else{
+        inputPass.setAttribute('type','password')
+        btnShowPass.classList.replace('bi-eye-slash','bi-eye')
+    }
+}
